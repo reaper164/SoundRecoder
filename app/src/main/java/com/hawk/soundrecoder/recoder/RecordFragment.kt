68888ba7754a9115a1d3cd0f271bbfc04c59a480
Nick Zhilyaev <nick.vachovski@mail.ru>
2021-01-15 16:10:47 +0300

    fragment_record button adding

--- app/src/main/java/com/hawk/soundrecoder/recoder/RecordFragment.kt
+++ app/src/main/java/com/hawk/soundrecoder/recoder/RecordFragment.kt
@@ -1,21 +1,150 @@
 package com.hawk.soundrecoder.recoder
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.hawk.soundrecoder.MainActivity
 import com.hawk.soundrecoder.R
+import com.hawk.soundrecoder.database.ReacordDatabaseDao
+import com.hawk.soundrecoder.database.RecordDatabase
+import com.hawk.soundrecoder.databinding.FragmentRecordBinding
+import java.io.File
 
 
 class RecordFragment : Fragment() {
+
+    private lateinit var viewModel: RecordViewModel
+    private lateinit var mainActivity: MainActivity
+    private var count : Int? = null
+    private var database : ReacordDatabaseDao? = null
+    private var MY_PERMISSIONS_RECORD_AUDIO = 123
+    private lateinit var playButton : FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false)
+
+        playButton = mainActivity.findViewById(R.id.playButton)
+
+        var binding = DataBindingUtil.inflate<FragmentRecordBinding>(
+                inflater,
+                R.layout.fragment_record,
+                container, false
+        )
+
+        database = context?.let { RecordDatabase.getInstance(it).recordDatabaseDao }
+
+        mainActivity = activity as MainActivity
+
+        viewModel = ViewModelProvider(this).get(RecordViewModel::class.java)
+
+        binding.recordViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        if (!mainActivity.isServiceRunning()) {
+            viewModel.resetTimer()
+        } else {
+            binding.playButton.setImageResource(R.drawable.ic_media_stop)
+        }
+
+        binding.playButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(requireContext(),
+                            android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(
+                        arrayOf(android.Manifest.permission.RECORD_AUDIO), MY_PERMISSIONS_RECORD_AUDIO)
+            } else {
+                if (mainActivity.isServiceRunning()) {
+                    onRecord(false)
+                    viewModel.stopTimer()
+                } else {
+                    onRecord(true)
+                    viewModel.startTimer()
+                }
+            }
+        }
+
+        createChannel(
+                getString(R.string.notification_channel_id),
+                getString(R.string.notification_channel_name)
+        )
+
+        return binding.root
+    }
+
+    private fun onRecord(start: Boolean) {
+        val intent: Intent =  Intent(activity, RecordService::class.java)
+
+        if (start) {
+            playButton.setImageResource(R.drawable.ic_media_stop)
+            Toast.makeText(activity, R.string.toast_recording_start, Toast.LENGTH_SHORT).show()
+
+            val folder = File(activity?.getExternalFilesDir(null)?.absolutePath.toString() + "/VoiceRecorder")
+            if (!folder.exists()) {
+                folder.mkdir()
+            }
+
+            activity?.startService(intent)
+            activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        } else {
+            playButton.setImageResource(R.drawable.ic_mic_white_36dp)
+
+            activity?.stopService(intent)
+            activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        when(requestCode) {
+            MY_PERMISSIONS_RECORD_AUDIO -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    onRecord(true)
+                    viewModel.startTimer()
+                } else {
+                    Toast.makeText(
+                            activity,
+                            getString(R.string.toast_recording_permissions),
+                            Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            )
+                    .apply {
+                        setShowBadge(false)
+                        setSound(null, null)
+                    }
+            val notificationManager = requireActivity().getSystemService(
+                    NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
     }
 
 }
