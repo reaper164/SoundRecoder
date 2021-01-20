b18ba942a2b56cfca2a8a8db095081b3af1e3158
Hawk <hawk@mail.ru>
2021-01-20 18:31:21 +0300

    Updated ListRecordViewModelFactory class

--- app/src/main/java/com/hawk/soundrecoder/listRecords/ListRecordViewModelFactory.kt
+++ app/src/main/java/com/hawk/soundrecoder/listRecords/ListRecordViewModelFactory.kt
@@ -3,9 +3,13 @@ package com.hawk.soundrecoder.listRecords
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.hawk.soundrecoder.database.ReacordDatabaseDao
+import java.lang.IllegalArgumentException
 
 class ListRecordViewModelFactory (private var databaseDao : ReacordDatabaseDao) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("Not yet implemented")
+        if(modelClass.isAssignableFrom(ListRecordViewModel::class.java)){
+            return ListRecordViewModel(databaseDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
